0002426400d35e72bc1c5b7059a1e802fe330c67
Andrii Martishyn <contactgently@gmail.com>
2026-04-11 13:53:48 +0300

    Added initial securebase project module

---
+++ Securebase/src/main/java/com/martishyn/SecurebaseApplication.java
@@ -0,0 +1,13 @@
+package com.martishyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurebaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurebaseApplication.class, args);
+    }
+
+}
